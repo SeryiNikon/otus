bdf897567becdaa41ef221fe3ff1b9d47d83efca
SeryiNikon <101667527+SeryiNikon@users.noreply.github.com>
2022-03-31 00:24:06 +0500

    Update base.py

--- homework_02/base.py
+++ homework_02/base.py
@@ -2,8 +2,17 @@ from abc import ABC
 
 
 class Vehicle(ABC):
+    def __init__ (self, weight, fuel, fuel_consumption)
+    self.weight = weight
+    self.fuel = fuel
+    self.fuel_consumption = fuel_consumption
     Vehicle.weight
     Vehicle.started
     Vehicle.fuel
     Vehicle.fuel_consumption
+    
+    def start(started):
+        for start in started:
+            
+        
     pass

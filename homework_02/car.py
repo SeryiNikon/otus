6511ebd87541c81e830f75b21b8e8fcd9e9e466e
SeryiNikon <101667527+SeryiNikon@users.noreply.github.com>
2022-04-02 00:03:58 +0500

    Update car.py

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,8 +1,10 @@
 from homework_02.base import Vehicle
+from homework_02.engine import Engine
 """
 создайте класс `Car`, наследник `Vehicle`
 """
 class Car(Vehicle):
+     engene: Engene
 
      def set_engine(self, engine):
          self.engine = engine

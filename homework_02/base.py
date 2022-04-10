c00152b41395020617262ba9267e1df4f0ffc68d
SeryiNikon <101667527+SeryiNikon@users.noreply.github.com>
2022-04-11 02:22:21 +0500

    Update base.py

--- homework_02/base.py
+++ homework_02/base.py
@@ -8,11 +8,18 @@ class TestVehicle(ABC):
         self.weight = weight
         self.fuel = fuel
         self.started = False
-
-    def __repr__(self):
-        return f'{__class__.__name__} (weight: {self.weight}, ' \
-               f'fuel: {self.fuel}, started: {self.started})'
-
+        
+    def start(self):
+    if not self.started:
+        if self.fuel > 0:
+            self.started = True
+            print('Двигатель заведен')
+        else:
+            raise LowFuelError
+    else:
+        print('Дивгатель уже заведен')
+        
+        
     def move(self, distance):
         max_distance = self.fuel / self.fuel_consumption
         if distance <= max_distance:

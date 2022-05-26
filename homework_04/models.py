1504e6f1f9df65a34ab51c1eecf4e3ef38db79ca
SeryiNikon <101667527+SeryiNikon@users.noreply.github.com>
2022-05-26 17:59:53 +0500

    Update models.py

--- homework_04/models.py
+++ homework_04/models.py
@@ -9,8 +9,42 @@
 """
 
 import os
+from distutils.command.config import config
+from tokenize import String
+
+from sqlalchemy import Integer, Column
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
 
 PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
+async_engine = create_async_engine(
+    config.PG_CONN_URI,
+    echo=True,
+)
 
-Base = None
-Session = None
+#Base = None
+Base = declarative_base(async_engine)
+
+#Session = None
+async_session = sessionmaker(
+    async_engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
+
+class User(Base):
+    __tablename__ = 'Users'
+    name = Column(String(100), nullable=False)
+    username = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False)
+    Posts = relationship("Post")
+    #dl = relationship('Post', backref='User', uselist=False)
+
+
+class Post(Base):
+    __tablename__ = 'Posts'
+    user_id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    body = Column(String(100), nullable=False)
+

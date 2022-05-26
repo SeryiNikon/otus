822116be39cec84d7f597e18c93cd3a0b5a5efac
SeryiNikon <101667527+SeryiNikon@users.noreply.github.com>
2022-05-26 20:26:29 +0500

    Update app.py

--- homework_03/blogproject/app.py
+++ homework_03/blogproject/app.py
@@ -1,8 +1,8 @@
-﻿from fastapi import FastAPI
+from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/ping/")
-def root():
-    return {"message": "pong"}
 
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}

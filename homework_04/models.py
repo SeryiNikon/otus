bc9d18b305721ebc1b63b6b5aed33c843ed8ed48
SeryiNikon <101667527+SeryiNikon@users.noreply.github.com>
2022-06-02 21:02:46 +0500

    Update models.py

--- homework_04/models.py
+++ homework_04/models.py
@@ -10,37 +10,40 @@
 
 import os
 
-from sqlalchemy import Integer, Column, String, ForeignKey
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from sqlalchemy import (
+    Text,
+    Column,
+    String,
+    Integer, ForeignKey,
+)
 
 PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
-async_engine = create_async_engine(
+
+engine = create_async_engine(
     PG_CONN_URI,
     echo=True,
 )
 
-Base = declarative_base(async_engine)
+Base = declarative_base()
+Session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
-async_session = sessionmaker(
-    async_engine,
-    expire_on_commit=False,
-    class_=AsyncSession,
-)
 
 class User(Base):
-    __tablename__ = 'Users'
-    name = Column(String(100), nullable=False)
-    username = Column(String(100), nullable=False)
-    email = Column(String(100), nullable=False)
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    username = Column(String)
+    email = Column(String)
+    user_id = Column(Integer)
     posts = relationship("Post", back_populates="user")
 
 
 class Post(Base):
-    __tablename__ = 'Posts'
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    title = Column(String(100), nullable=False)
-    body = Column(String(100), nullable=False)
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    title = Column(String)
+    body = Column(Text)
     user = relationship("User", back_populates="posts")
-    

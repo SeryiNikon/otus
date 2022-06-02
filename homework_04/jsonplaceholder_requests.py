d065c424d860bb8806931a4f84daa06ef9c59b75
SeryiNikon <101667527+SeryiNikon@users.noreply.github.com>
2022-06-02 18:26:35 +0500

    Update jsonplaceholder_requests.py

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -1,7 +1,7 @@
 """
 создайте асинхронные функции для выполнения запросов к ресурсам (используйте aiohttp)
 """
-import asyncio
+from typing import Dict
 
 import aiohttp
 
@@ -9,12 +9,25 @@ USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
 POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
 
 
-async def fetch_json(url):
+async def get_userdata() -> Dict:
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            return await response.json()
+        async with session.get(USERS_DATA_URL) as resp:
+            userdata: dict = await resp.json()
+            return userdata
 
 
-if __name__ == '__main__':
-    asyncio.run(fetch_json(USERS_DATA_URL))
-    asyncio.run(fetch_json(POSTS_DATA_URL))
+async def get_posts() -> Dict:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(POSTS_DATA_URL) as resp:
+            postdata: dict = await resp.json()
+            return postdata
+
+#async def fetch_json(url):
+#    async with aiohttp.ClientSession() as session:
+#        async with session.get(url) as response:
+#            return await response.json()
+
+
+#if __name__ == '__main__':
+#    asyncio.run(fetch_json(USERS_DATA_URL))
+#    asyncio.run(fetch_json(POSTS_DATA_URL))

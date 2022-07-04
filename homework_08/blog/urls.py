4d1e39cf94a5f68b825df36ca7a46216da971e48
SeryiNikon <101667527+SeryiNikon@users.noreply.github.com>
2022-07-04 20:08:59 +0500

    Update urls.py

--- homework_08/blog/urls.py
+++ homework_08/blog/urls.py
@@ -4,6 +4,5 @@ from blog.views import PostListView, PostDetailView
 
 
 urlpatterns = [
-    path('/', PostListView.as_view(), name='post-list'),
-    path('/<slug:slug>/', PostDetailView.as_view(), name='article-detail'),
+
 ]

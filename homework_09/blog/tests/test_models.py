ce99f9bfd9541b751d4a67959e6d2c46c5d16c26
SeryiNikon <101667527+SeryiNikon@users.noreply.github.com>
2022-07-11 18:37:45 +0500

    Add files via upload

---
+++ homework_09/blog/tests/test_models.py
@@ -0,0 +1,22 @@
+import unittest
+
+from django.test import TestCase
+
+class PostTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @classmethod
+    def TearDawnClass(cls):
+        pass
+
+    if __name__ =='__main__':
+        unittest.main(verbosity=2)
---
+++ homework_09/blog/tests/test_views.py
@@ -0,0 +1,22 @@
+import unittest
+from unittest import TestCase
+
+
+class PostListViewsTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @classmethod
+    def TearDawnClass(cls):
+        pass
+
+    if __name__ == '__main__':
+        unittest.main(verbosity=2)

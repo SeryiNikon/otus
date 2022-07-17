3e8f25168008987a354d51eedf49568eb7bee191
SeryiNikon <101667527+SeryiNikon@users.noreply.github.com>
2022-07-17 15:50:28 +0500

    Update test_views.py

--- homework_09/blog/tests/test_views.py
+++ homework_09/blog/tests/test_views.py
@@ -11,5 +11,5 @@ class PostListViewsTest(TestCase):
         view = PostDetailView()
         view.setup(request)
 
-    if __name__ == '__main__':
-        unittest.main(verbosity=2)
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
